b44b95d524d3b1388f498fd232855114a34e3968
Govind shreelochan <gomac2507@gmail.com>
2025-04-19 18:10:13 +0530

    fixed env var initialisation

--- company-management-system/client/vite.config.js
+++ company-management-system/client/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import dotenv from 'dotenv';
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), dotenv.config()],
   server: {
     port: 5173,
     proxy: {
-      '/api': process.env.BACKEND_URL
+      '/api': process.env.VITE_BACKEND_URL
     }
   },
 });
