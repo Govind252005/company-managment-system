7e624e3229eddcf95d7d217600b54a4e19b5ff64
Govind shreelochan <gomac2507@gmail.com>
2025-04-19 20:12:43 +0530

    added host in vite config

--- company-management-system/client/vite.config.js
+++ company-management-system/client/vite.config.js
@@ -6,7 +6,8 @@ import dotenv from 'dotenv';
 export default defineConfig({
   plugins: [react(), tailwindcss(), dotenv.config()],
   server: {
-    port: 5173,
+    host: process.env.HOST || '0.0.0.0',
+    port: Number(process.env.PORT) || 5173,
     proxy: {
       '/api': process.env.VITE_BACKEND_URL
     }
